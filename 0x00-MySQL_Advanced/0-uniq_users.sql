729499d3c2764a1e7c684e681a2609f53737601e
NdiranguAndrew43 <andymurunga@gmail.com>
2024-10-17 20:40:43 +0000

    Modified to fix task requirements

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- This is a script to create a table users 
+-- according to these requirements
+-- attributes: id, email, name
+-- conditions: if table already exists script should fail  , must work on any database
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
